0caca1e3ec2a245b2f42bd3f23767f9fb0c8de21
Blaon <iloveelevation@gmail.com>
2020-04-27 20:50:04 +0200

    Text/price for items are done

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -40,17 +40,17 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(accessories);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", swords, fantasy));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", swords, sciFi));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", swords, fantasy));
-        productDataStore.add(new Product("Amazon F", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", vehicle, fantasy));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 0", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", vehicle, sciFi));
-        productDataStore.add(new Product("Amazon Fire HD", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", vehicle, fantasy));
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", guns, fantasy));
-        productDataStore.add(new Product("Len IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", guns, sciFi));
-        productDataStore.add(new Product("Aazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", guns, fantasy));
-        productDataStore.add(new Product("Amazoe", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", accessories, fantasy));
-        productDataStore.add(new Product("Lo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", accessories, sciFi));
-        productDataStore.add(new Product("mazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", accessories, fantasy));
+        productDataStore.add(new Product("Kylo Ren Lightsaber", 500f, "USD", "You can be the envoy of the dark side with this beauty!", swords, fantasy));
+        productDataStore.add(new Product("Legend of Zelda Ocarine of Time! Master Sword", 99.99f, "USD", "Can't miss sword from your Zelda Collection", swords, fantasy));
+        productDataStore.add(new Product("Alduril", 300, "USD", "Be a great king like Aragorn with his magical sword!", swords, fantasy));
+        productDataStore.add(new Product("Millenium Falcon", 1000, "USD", "Maybe old, maybe not the prettiest, but sure fly like hell!", vehicle, fantasy));
+        productDataStore.add(new Product("Enterprise",980.50f , "USD", "Go boldly where no man has ever gone before with you own spaceship!", vehicle, sciFi));
+        productDataStore.add(new Product("Götterdammerung", 760, "USD", "Don't have to be a dictator to look good in this!", vehicle, sciFi));
+        productDataStore.add(new Product("The Good Samaritan", 278, "USD", "One HELL of a kickback!", guns, fantasy));
+        productDataStore.add(new Product("Usopp's Kabuto Slingshot", 20, "USD", "There is literally only One Piece we have of this!", guns, fantasy));
+        productDataStore.add(new Product("Han Solo's Blaster", 430.50f, "USD", "Easily concealed under the table", guns, fantasy));
+        productDataStore.add(new Product("Mario's Hat", 15, "USD", "Accessory for plumbers", accessories, fantasy));
+        productDataStore.add(new Product("Xenomorph's Tail", 80, "USD", "Long,nimble,deadly!", accessories, sciFi));
+        productDataStore.add(new Product("DragonBall Balls", 700, "USD", "You don't need to search the world for a wish", accessories, fantasy));
     }
 }
