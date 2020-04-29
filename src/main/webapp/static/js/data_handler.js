9a84a78e2ec15c94a86171985047d1e59e1df0b7
Tóth Máté <tothmate911@gmail.com>
2020-04-29 11:43:36 +0200

    add to cart

---
+++ src/main/webapp/static/js/data_handler.js
@@ -0,0 +1,24 @@
+export let dataHandler = {
+    _api_get: function (url, callback) {
+        fetch(url, {
+            method: 'GET',
+            credentials: 'same-origin'
+        })
+            .then(response => response.json())  // parse the response as JSON
+            .then(json_response => callback(json_response));  // Call the `callback` with the returned object
+    },
+
+    _api_post: function (url, data, callback) {
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(function () {
+                callback()
+            })
+    }
+};
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,5 +1,13 @@
+import {dataHandler} from "./data_handler.js";
+
 function main() {
-    let addToCartButtons = document.querySelectorAll();
+    let addToCartButtons = document.querySelectorAll(".add-to-cart-button");
+    for (let button of addToCartButtons) {
+        button.addEventListener("click", function () {
+            dataHandler._api_post("/cart", 1, )
+            alert("ho");
+        })
+    }
 }
 
 main();
