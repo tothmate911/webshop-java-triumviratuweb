fefdb6a3eed2772e2e3d1577cda25669d4128718
Pivi14 <pivarcsiadam@gmail.com>
2020-04-29 11:20:15 +0200

    js implemented

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,6 +1,53 @@
-import { dataHandler } from "./data_handler.js";
+function addElement(id) {
+    let sendData = {
+        "id": id
+    };
+    addCounter(id)
+    // api_get("/addToCart")
+}
 
-function addElement() {
-    let sendData = {"id": this.dataset.id};
-    alert(sendData);
+function removeElement(id) {
+    let sendData = {
+        "id": id
+    }
+    removeCounter(id)
+}
+
+function addCounter(id) {
+    let productCount = document.getElementById("count" + id);
+    productCount.innerText = (parseInt(productCount.innerText) + 1).toString();
+}
+
+function removeCounter(id) {
+    let productCount = document.getElementById("count" + id);
+    productCount.innerText = (parseInt(productCount.innerText) - 1).toString();
+    if (productCount.innerText === "0"){
+        document.getElementById(id).remove();
+    }
+}
+
+
+
+
+
+function api_get(url, callback) {
+    fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+        .then(response => response.json())
+        .then(json_response => callback(json_response));
+}
+
+function api_post(url, data, callback) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(function () {
+            callback()
+        })
 }
