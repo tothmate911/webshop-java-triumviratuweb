5f4d1c14cc17afb4411efeb635ae5d6f07461644
Blaon <iloveelevation@gmail.com>
2020-05-14 16:28:37 +0200

    test supplier

--- src/test/java/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
@@ -1,8 +1,11 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.model.Supplier;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.postgresql.util.PSQLException;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,20 +13,33 @@ public class SupplierDaoMemTest {
     private SupplierDaoMem supplierDaoMem = SupplierDaoMem.getInstance();
 
     @Test
-    public void testFind() {
+    public void testFind(){
         Supplier supplier = supplierDaoMem.find(1);
         assertEquals("Fantasy", supplier.getName());
         assertEquals("Only Fantasy", supplier.getDescription());
     }
 
     @Test
-    @Sql({"webshop_test.sql"})
-    public void testAdd() {
+    public void testAdd(){
         Supplier supplier = new Supplier("P&T", "Fantasy weapon dealership");
         supplierDaoMem.add(supplier);
         Supplier foundSupplier = supplierDaoMem.find(3);
         assertEquals("P&T", foundSupplier.getName());
         assertEquals("Fantasy weapon dealership", foundSupplier.getDescription());
     }
+    @Test
+    public void testRemove(){
+        SupplierDaoMem supplierDaoMem = SupplierDaoMem.getInstance();
+        supplierDaoMem.remove(3);
+    }
+
+    @Test
+    public void testGetAll(){
+
+
+    }
 
 }
+
+
+
