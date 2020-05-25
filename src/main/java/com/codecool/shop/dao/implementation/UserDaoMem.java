5c7be3178acbe19e2bdb19b27cdfad88fa8e1452
Tóth Máté <tothmate911@gmail.com>
2020-05-25 14:46:18 +0200

    rename password to hashed_password

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -1,14 +1,29 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.controller.DbConnect;
 import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 
 import java.util.List;
 
 public class UserDaoMem implements UserDao {
+    private DbConnect dbConnect = DbConnect.getDbConnect();
+    private static UserDaoMem userDaoMemInstance;
+
+    private UserDaoMem() {
+    }
+
+    public UserDaoMem getInstance() {
+        if (userDaoMemInstance == null) {
+            userDaoMemInstance = new UserDaoMem();
+        }
+        return userDaoMemInstance;
+    }
 
     @Override
     public void add(User user) {
+        String query = "INSERT INTO web_user VALUES (?, ?, )";
+
 
     }
 
