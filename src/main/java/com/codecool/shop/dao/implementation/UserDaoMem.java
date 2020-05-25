abca923a67412bb0084222eb0cd5d442b4a97a7b
Tóth Máté <tothmate911@gmail.com>
2020-05-25 14:28:43 +0200

    create UserDao

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -1,6 +1,29 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.User;
+
+import java.util.List;
 
 public class UserDaoMem implements UserDao {
+
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public User find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
 }
--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -1,4 +1,14 @@
 package com.codecool.shop.model;
 
 public class User {
+    private String username;
+    private String emailAddress;
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public User(String username, String emailAddress) {
+        this.username = username;
+        this.emailAddress = emailAddress;
+    }
 }
