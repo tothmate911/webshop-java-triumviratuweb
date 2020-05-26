37bc5873f9babc26a29677174f3c79e824ea4969
Tóth Máté <tothmate911@gmail.com>
2020-05-26 10:06:02 +0200

    start implementing UserDaoMem

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -4,13 +4,19 @@ import com.codecool.shop.controller.DbConnect;
 import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoMem implements UserDao {
-    private DbConnect dbConnect = DbConnect.getDbConnect();
+    private DataSource dataSource;
     private static UserDaoMem userDaoMemInstance;
 
     private UserDaoMem() {
+        dataSource = DbConnect.getDbConnect().getDataSource();
     }
 
     public UserDaoMem getInstance() {
@@ -22,13 +28,37 @@ public class UserDaoMem implements UserDao {
 
     @Override
     public void add(User user) {
-        String query = "INSERT INTO web_user VALUES (?, ?, )";
+        String query = "INSERT INTO web_user (user_name, email, hashed_password, user_is_active) " +
+                "VALUES (?, ?, ?, FALSE)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getEmailAddress());
+            preparedStatement.setString(3, user.getHashedPassword());
 
-
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public User find(int id) {
+        String query = "SELECT * FROM web_user WHERE user_id = ?;";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+//                    User user = new User();
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 
--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -5,10 +5,40 @@ public class User {
     private String emailAddress;
     private String firstName;
     private String lastName;
-    private int id;
+    private String hashedPassword;
+    private Integer id;
 
-    public User(String username, String emailAddress) {
+    public User(String username, String emailAddress, String hashedPassword) {
         this.username = username;
         this.emailAddress = emailAddress;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
     }
 }
