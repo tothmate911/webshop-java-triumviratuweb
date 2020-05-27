102e2e137b0921ae9dc243fcb3f60fa9a8a1070c
Blaon <iloveelevation@gmail.com>
2020-05-27 13:48:07 +0200

    Logout is working

---
+++ src/main/java/com/codecool/shop/controller/Logout.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import org.mindrot.jbcrypt.BCrypt;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/logout"})
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        session.invalidate();
+        resp.sendRedirect("/");
+    }
+}
